40f03b5b31dc14298c24f1f06df9026a08a2ff3a
M. Kamil <mk.kamiil@gmail.com>
2021-11-22 17:26:59 +0700

    fix

--- src/main/java/com/jwt/userservice/model/entity/Role.java
+++ src/main/java/com/jwt/userservice/model/entity/Role.java
@@ -5,12 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Builder
